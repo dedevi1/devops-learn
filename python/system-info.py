40d7fb5d7a2289cfa5eddb8ebcbbe72325d14d20
dedevi1 <261859008+dedevi1@users.noreply.github.com>
2026-04-15 13:46:01 +0200

    Add system info print statements
    Fix typos and add system information prints.

--- python/system-info.py
+++ python/system-info.py
@@ -1,3 +1,11 @@
 import os
 import platform
 import datetime
+
+print("==== SYSTEM INFO ====")
+print(f"Hostname:  {platform.node()}")
+print(f"OS:  {platform.system()} {latform.release()}")
+print(f"Datum:  {platform.datetime.now().strftime('%d.%m.%Y %H.%M')}")
+print(f"CPU Kerne:  {platform.cpu_xount()}")
+print(f"Python:  {platform.python.version()}")
+print("============================")
