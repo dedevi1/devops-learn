8d62ec9b787875c2cebe5e8c439840d807885ac9
dedevi1 <261859008+dedevi1@users.noreply.github.com>
2026-04-15 13:49:37 +0200

    Update system-info.py

--- python/system-info.py
+++ python/system-info.py
@@ -4,8 +4,8 @@ import datetime
 
 print("==== SYSTEM INFO ====")
 print(f"Hostname:  {platform.node()}")
-print(f"OS:  {platform.system()} {latform.release()}")
-print(f"Datum:  {platform.datetime.now().strftime('%d.%m.%Y %H.%M')}")
-print(f"CPU Kerne:  {platform.cpu_xount()}")
-print(f"Python:  {platform.python.version()}")
+print(f"OS:  {platform.system()} {platform.release()}")
+print(f"Datum:  {datetime.datetime.now().strftime('%d.%m.%Y %H.%M')}")
+print(f"CPU Kerne:  {os.cpu_count()}")
+print(f"Python:  {platform.python_version()}")
 print("============================")
